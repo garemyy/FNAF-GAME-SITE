4ce7b24d2471f3589420030ae0570825b3d20d65
Garemy <garemyisthebest@email.com>
2025-12-09 17:19:00 +0100

    Aggiornamento build per GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,16 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
+  base: "/FNAF-GAME-SITE/", // <- aggiungi questa riga
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react()], // rimosso componentTagger()
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
